7879ed6ba7d6d1a402994b67dc15798fa607aef2
Projek Dwi <dwirifanii127@gmail.com>
2023-04-12 02:13:02 -0600

    update

---
+++ Projek Dwi/Komentas.js
@@ -0,0 +1,18 @@
+    
+    
+
+    // Ini merupakan Komentar satu baris, dibawah ini tidak akan dijalankan 
+    // Console.log("Hallo");
+    //Nama : Dwi Rifami
+    //Kelas : RPL A
+
+
+    /* Ini merupakan komentar dengan lebih dari satu baris 
+    Teks apapun yang berada disini akan dijalankan komentar.
+    Ketika menggunakan ini, jangan lupa untuk menutupnya.
+    */
+   /* Aq lahir pada tanggal 18 juni 2002,
+   lahir pada rasi bintang leo. Hobiku Hiking, karena hiking mengajarkan akan 
+   solidaritas & arti teman yang sesungguhnya*/
+
+    
---
+++ Projek Dwi/Var_Let_Const.js
@@ -0,0 +1,23 @@
+//Menggunakan program var
+
+var nama = "Dwi";
+console.log("Hallo, nama saya " + nama );
+nama = "Rifani"
+console.log("Sekarang nama saya " + nama);
+
+//Menggunakan program let 
+function printMessage() {
+    let message = "Nama saya Dwi";
+    console.log(message);
+}
+printMessage();
+
+//Menggunakan program const
+const aku = {
+    Nama :"Dwi",
+    Umur : 20,
+    Askot : "Jepara"
+
+};
+console.log(aku.Nama + "nm" + aku.Umur + "Ur" + aku.Askot );
+
